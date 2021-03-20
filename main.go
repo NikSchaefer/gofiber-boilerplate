378c119374d25c72be4593f9a430cb0ac9e1fb89
NikSchaefer <nikkschaefer@gmail.com>
2021-03-20 12:51:14 -0500

    readd main.go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := fiber.New()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // comma format e.g. "localhost, nikschaefer.tech"
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods: "GET,POST,PUT,DELETE",
+	}))
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.AutoMigrate(&User{}, &Session{})
+
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).SendString("Hello, World!")
+	})
+
+	AuthRoutes(router, db)
+
+	log.Fatal(router.Listen(":3000"))
+}
