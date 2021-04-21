1d20093117f8cee5e3fc82c17f816b2b6d19405c
NikSchaefer <nikkschaefer@gmail.com>
2021-04-20 22:56:17 -0500

    doc: Database Documentation

--- database/connect.go
+++ database/connect.go
@@ -10,7 +10,9 @@ import (
 )
 
 func ConnectDB() {
-	var err error
+	var err error // define error here to prevent overshadowing the global ConnectDB
+
+	// DATABASE_URL should be a connection string or url of a postgres database
 	DB, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
