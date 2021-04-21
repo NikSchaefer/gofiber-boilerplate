dce0286c70af98e1c5a4ed3ca40eca2ba7eb227b
NikSchaefer <nikkschaefer@gmail.com>
2021-04-21 15:26:16 -0500

    feat: secure and access-control headers

--- middleware/security.go
+++ middleware/security.go
@@ -9,5 +9,9 @@ func Security(c *fiber.Ctx) error {
 	c.Set("Strict-Transport-Security", "max-age=5184000")
 	c.Set("X-Frame-Options", "DENY")
 	c.Set("X-DNS-Prefetch-Control", "off")
+	c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
+	if !c.Secure() {
+		c.Status(400).SendString("Insecure Request")
+	} 
 	return c.Next()
 }
