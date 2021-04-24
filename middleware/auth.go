0e4c5cd86c6d5f4c0e265a6faff272e5d334d2af
NikSchaefer <nikkschaefer@gmail.com>
2021-04-24 18:34:49 -0500

    didn't solve back to normal

--- middleware/auth.go
+++ middleware/auth.go
@@ -12,10 +12,7 @@ func Authenticated(c *fiber.Ctx) error {
 	}
 	json := new(AuthRequest)
 	if err := c.BodyParser(json); err != nil {
-		return c.Status(422).JSON(fiber.Map{
-			"err":  err,
-			"body": c.Body(),
-		})
+		return c.Status(422).JSON(err)
 	}
 	user, status := handlers.GetUser(json.Sessionid)
 	if status != 0 {
@@ -24,3 +21,34 @@ func Authenticated(c *fiber.Ctx) error {
 	c.Locals("user", user)
 	return c.Next()
 }
+
+/*
+func Authenticated(c *fiber.Ctx) error {
+	type AuthRequest struct {
+		Sessionid guuid.UUID `json:"sessionid"`
+	}
+	js := new(AuthRequest)
+	b := c.Body()
+	if err := json.Unmarshal(c.Body(), &js); err != nil {
+		var msg string
+		switch t := err.(type) {
+		case *json.SyntaxError:
+			jsn := string(b[0:t.Offset])
+			jsn += "<--(Invalid Character)"
+			msg = fmt.Sprintf("Invalid character at offset %v\n %s", t.Offset, jsn)
+		case *json.UnmarshalTypeError:
+			jsn := string(b[0:t.Offset])
+			jsn += "<--(Invalid Type)"
+			msg = fmt.Sprintf("Invalid value at offset %v\n %s", t.Offset, jsn)
+		default:
+			msg = err.Error()
+		}
+		return c.Status(200).JSON(fiber.Map{
+			"err":  err.Error(),
+			"data": js,
+			"msg":  msg,
+		})
+
+	}
+
+*/
