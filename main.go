bccf5bac7782e52ab47309fcb352fb050e542dbb
NikSchaefer <nikkschaefer@gmail.com>
2021-04-25 18:02:58 -0500

    Load Config from Fiber-Lambda Repository

--- main.go
+++ main.go
@@ -31,23 +31,31 @@ func main() {
 	database.ConnectDB()
 
 	router.Initalize(app)
-
 	log.Fatal(app.Listen(":" + getenv("PORT", "3000")))
 }
 
 /*
-Set Env variables, PORT will auto set to 3000 if not set
-*
+ENV Variables:
+will auto set to 3000 if not set
 PORT=3000
+this should be a connection string or url
 DATABASE_URL="host=localhost port=5432 user=postgres password= dbname= sslmode=disable"
-*/
-
-/*
-Docker Command for Postgres database
-*
+**
+Docker Command for Postgres database:
 docker run --name database -d -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:alpine
-*
+
 DB_URL Variable for docker database
-*
 DATABASE_URL="host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
+**
+Docker build base image in first stage for development
+docker build --target build -t base .
+**
+run dev container
+docker run -p 3000:3000 --mount type=bind,source="C:\Users\schaefer\go\src\fiber",target=/go/src/app --name fiber -td base
+**
+rebuild and run package
+docker exec -it web go run main.go
+**
+stop and remove container
+docker stop fiber; docker rm fiber
 */
